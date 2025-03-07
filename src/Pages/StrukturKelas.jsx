56e3901bd7ad13d347eebf12576401d758631ff3
EvanFadly <evanfadly4321@gmail.com>
2025-03-07 20:18:18 +0700

    update bendahara in strukturKelas

--- src/Pages/StrukturKelas.jsx
+++ src/Pages/StrukturKelas.jsx
@@ -136,7 +136,7 @@ const StrukturKelas = () => {
 					<div className="flex-col">
 						<BorderStruktur Jabatan="Bendahara" Nama="Zadid" Width="120px" />
 						<div className="py-[3%]"></div>
-						<BorderStruktur Jabatan="" Nama="Neysa" Width="120px" />
+						<BorderStruktur Jabatan="" Nama="Neisya" Width="120px" />
 					</div>
 				</div>
 
